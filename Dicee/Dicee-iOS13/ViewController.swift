2d947da2c194d00d7fcbd8b73850d1d470f3ca31
Sam Liu <o4d0b@outlook.com>
2020-11-05 01:31:14 -0800

    Initial Commit

---
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // IBOutlets to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArr = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        // randomElement == Int.random(in: 0...5)
+        diceImageView1.image = diceArr.randomElement()
+        diceImageView2.image = diceArr.randomElement()
+        
+    }
+    
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# swift-5-learning
+A repo for my  iOS 13 with swift 5 learning projects.
